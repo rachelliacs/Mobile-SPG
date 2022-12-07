9e895e0328a2627e2614bdd63e050234703961c3
rachelliacs <rachellia.citra13@gmail.com>
2022-12-07 17:40:23 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/mobile_spgapp/generatedopenmenuwidget/GeneratedOpenMenuWidget.dart';
+import 'package:flutterapp/mobile_spgapp/generatedhomepagewidget/GeneratedHomepageWidget.dart';
+import 'package:flutterapp/mobile_spgapp/generatedartikelwidget2/GeneratedArtikelWidget2.dart';
+import 'package:flutterapp/mobile_spgapp/generatedbuttonwidget12/GeneratedButtonWidget12.dart';
+import 'package:flutterapp/mobile_spgapp/generatedverifikasinilaiwidget2/GeneratedVerifikasiNilaiWidget2.dart';
+import 'package:flutterapp/mobile_spgapp/generatedpendaftaran1widget/GeneratedPendaftaran1Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddatapendaftaranwidget/GeneratedDataPendaftaranWidget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddpjurusan1widget/GeneratedDPJurusan1Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddpjurusan2widget/GeneratedDPJurusan2Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddpjurusan3widget/GeneratedDPJurusan3Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddpjurusan4widget/GeneratedDPJurusan4Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddpjurusan5widget/GeneratedDPJurusan5Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddpjurusan6widget/GeneratedDPJurusan6Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddpjurusan7widget/GeneratedDPJurusan7Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddpjurusan8widget/GeneratedDPJurusan8Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generateddpjurusan9widget/GeneratedDPJurusan9Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generatedperingatankirimdata1widget/GeneratedPeringatanKirimData1Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generatedperingatanpendaftaranwidget/GeneratedPeringatanPendaftaranWidget.dart';
+import 'package:flutterapp/mobile_spgapp/generatednilai80widget/GeneratedNilai80Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generatednilai75widget/GeneratedNilai75Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generatednilai85widget/GeneratedNilai85Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generatednilai90widget/GeneratedNilai90Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generatednilai95widget/GeneratedNilai95Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generatednis2widget/GeneratedNIS2Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generatednama2widget/GeneratedNama2Widget.dart';
+import 'package:flutterapp/mobile_spgapp/generatedcheckwidget12/GeneratedCheckWidget12.dart';
+import 'package:flutterapp/mobile_spgapp/generatedcomponent1widget/GeneratedComponent1Widget.dart';
+
+void main() {
+  runApp(mobile_spgApp());
+}
+
+class mobile_spgApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedLogin1Widget',
+      routes: {
+        '/GeneratedOpenMenuWidget': (context) => GeneratedOpenMenuWidget(),
+        '/GeneratedHomepageWidget': (context) => GeneratedHomepageWidget(),
+        '/GeneratedArtikelWidget2': (context) => GeneratedArtikelWidget2(),
+        '/GeneratedButtonWidget12': (context) => GeneratedButtonWidget12(),
+        '/GeneratedVerifikasiNilaiWidget2': (context) =>
+            GeneratedVerifikasiNilaiWidget2(),
+        '/GeneratedPendaftaran1Widget': (context) =>
+            GeneratedPendaftaran1Widget(),
+        '/GeneratedDataPendaftaranWidget': (context) =>
+            GeneratedDataPendaftaranWidget(),
+        '/GeneratedDPJurusan1Widget': (context) => GeneratedDPJurusan1Widget(),
+        '/GeneratedDPJurusan2Widget': (context) => GeneratedDPJurusan2Widget(),
+        '/GeneratedDPJurusan3Widget': (context) => GeneratedDPJurusan3Widget(),
+        '/GeneratedDPJurusan4Widget': (context) => GeneratedDPJurusan4Widget(),
+        '/GeneratedDPJurusan5Widget': (context) => GeneratedDPJurusan5Widget(),
+        '/GeneratedDPJurusan6Widget': (context) => GeneratedDPJurusan6Widget(),
+        '/GeneratedDPJurusan7Widget': (context) => GeneratedDPJurusan7Widget(),
+        '/GeneratedDPJurusan8Widget': (context) => GeneratedDPJurusan8Widget(),
+        '/GeneratedDPJurusan9Widget': (context) => GeneratedDPJurusan9Widget(),
+        '/GeneratedPeringatanKirimData1Widget': (context) =>
+            GeneratedPeringatanKirimData1Widget(),
+        '/GeneratedPeringatanPendaftaranWidget': (context) =>
+            GeneratedPeringatanPendaftaranWidget(),
+        '/GeneratedNilai80Widget': (context) => GeneratedNilai80Widget(),
+        '/GeneratedNilai75Widget': (context) => GeneratedNilai75Widget(),
+        '/GeneratedNilai85Widget': (context) => GeneratedNilai85Widget(),
+        '/GeneratedNilai90Widget': (context) => GeneratedNilai90Widget(),
+        '/GeneratedNilai95Widget': (context) => GeneratedNilai95Widget(),
+        '/GeneratedNIS2Widget': (context) => GeneratedNIS2Widget(),
+        '/GeneratedNama2Widget': (context) => GeneratedNama2Widget(),
+        '/GeneratedCheckWidget12': (context) => GeneratedCheckWidget12(),
+        '/GeneratedComponent1Widget': (context) => GeneratedComponent1Widget(),
+      },
+    );
+  }
+}
